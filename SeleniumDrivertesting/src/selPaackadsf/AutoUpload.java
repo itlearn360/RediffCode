e56967c4baaf5fae05555876e8be988ec178041c
Sushil Kushwah <sushil@dhiyotech.in>
2023-11-02 02:38:28 +0530

    UploadCode

--- SeleniumDrivertesting/src/selPaackadsf/AutoUpload.java
+++ SeleniumDrivertesting/src/selPaackadsf/AutoUpload.java
@@ -2,7 +2,6 @@ package selPaackadsf;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,48 +9,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutoUpload {
 public static void main(String[] args) throws IOException, InterruptedException {
 	
-	ChromeDriver d = new ChromeDriver();
-	   d.manage().window().maximize();
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
 	   // for page load
-	   d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");       // Testing webpage
-	   d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);      // for Implicit wait
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");       
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);      
 	             
-	   WebElement uname = d.findElement(By.name("username"));          // Username.........ID
+	   WebElement uname = driver.findElement(By.name("username"));         
 	   uname.sendKeys("Admin");
 	     
-	   WebElement pwd = d.findElement(By.name("password"));    // Password.........NAME
+	   WebElement pwd = driver.findElement(By.name("password"));   
 	   pwd.sendKeys("admin123");
 	    
-	   WebElement loginb = d.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
-	   loginb.click();                                   // Loginbutton......XPATH
+	   WebElement loginb = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+	   loginb.click();                                 
 	         
 	 
-	   WebElement pim = d.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span"));
-	    pim.click();                                            // Admin tab-PIM
+	   WebElement pim = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span"));
+	    pim.click();                                            
 	 
-	   WebElement config = d.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span"));
-	    config.click();                                  //Configuration tab
+	   WebElement config = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/span"));
+	    config.click();                               
 	 
-	    WebElement data_imp = d.findElement(By.partialLinkText("Data Import"));
-	    data_imp.click();                                 //Data Import tab.....PARTIALLINKT
+	    WebElement data_imp =driver.findElement(By.partialLinkText("Data Import"));
+	    data_imp.click();                                 
 	         
 	 
 	    // UPLOADING FILE USING AutoIT....
 	     
-	    WebElement browser = d.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div/div/div[2]/div/div[1]"));   //Browse button
+	    WebElement browser = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div/div/div[2]/div/div[1]"));   //Browse button
 	    browser.click();                                
-	    System.out.println("1");
+	    Runtime.getRuntime().exec("C:\\Users\\truba\\Desktop\\sample.exe");
 	 
-	 
-	  Runtime.getRuntime().exec("C:\\Users\\truba\\Desktop\\sample.exe");
-	  System.out.println("2");
 	  Thread.sleep(3000);
 	 
-	  WebElement upload = d.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button"));            //Upload button
+	  WebElement upload =driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button"));            //Upload button
 	   upload.click();
-	   System.out.println("3");     
+	       
 	  
 	   System.out.println("File Uploaded Successfully"); 
-	   d.close();
+	   driver.close();
 }
 }
---
+++ SeleniumDrivertesting/src/selPaackadsf/Screenshots.java
@@ -0,0 +1,64 @@
+package selPaackadsf;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.TakesScreenshot;
+
+public class Screenshots {
+
+public static WebDriver browserObject;
+	@Test
+	public  void screeshotTest() throws IOException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.chromedriver", "C://Program Files//chromedriver.exe");
+		 browserObject = new ChromeDriver ();
+		browserObject.get("http://demo.itlearn360.com/");
+		browserObject.manage().window().maximize();
+		browserObject.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+		
+		browserObject.findElement(By.xpath("//*[@id=\"loginlabel\"]")).click();
+		
+		String username= "Demo";
+		String password="Test123456$";
+		
+		
+		WebElement email= browserObject.findElement(By.id("user_login"));
+		email.sendKeys(username);
+		
+		WebElement pass= browserObject.findElement(By.id("user_pass"));
+		pass.sendKeys(password);
+		
+		
+		browserObject.findElement(By.id("wp-submit")).click();
+		
+		if(username.equals("Demo12"))
+		{
+			capureScreen();
+			System.out.println("Test Passed");
+			Assert.assertTrue(false);
+			
+		}
+		
+}
+	
+	public static void capureScreen() throws IOException
+	{
+		System.out.println("Screenshot section");
+		File sc= ((TakesScreenshot)browserObject).getScreenshotAs(OutputType.FILE);
+		Files.copy(sc, new File("C:\\Users\\truba\\Desktop\\sample\\username.jpg"));
+	}
+
+}
